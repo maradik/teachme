2ef3697c41290015f8acbff8b91812541a2a0356
maradik <maradik@bk.ru>
2016-10-19 23:24:41 +0600

    Оповещение пользователя Teacher через СМС при действиях над задачей

---
+++ TeachMe/Services/Jobs/JobActionHandlers/TeacherNotificationJobActionHandler.cs
@@ -0,0 +1,39 @@
+﻿using TeachMe.Helpers.Settings;
+using TeachMe.Models.Jobs;
+using TeachMe.Models.Users;
+using TeachMe.Services.Notifications;
+using TeachMe.Extensions;
+
+namespace TeachMe.Services.Jobs.JobActionHandlers
+{
+    public class TeacherNotificationJobActionHandler : IJobActionCustomHandler
+    {
+        private readonly ISmsService smsService;
+
+        public TeacherNotificationJobActionHandler(ISmsService smsService)
+        {
+            this.smsService = smsService;
+        }
+
+        public void Handle(Job job, JobActionType actionType, ApplicationUser user)
+        {
+            switch (actionType)
+            {
+                case JobActionType.Accept:
+                    NotificateUser(job.GetStudentUser(), $"Поступила оплата за задачу {(int)job.TeacherCost} руб.");
+                    break;
+                case JobActionType.OfferAbort:
+                    NotificateUser(job.GetStudentUser(), "Заказчик предлагает отменить Вашу задачу.");
+                    break;
+            }
+        }
+
+        private void NotificateUser(ApplicationUser user, string text)
+        {
+            if (!string.IsNullOrEmpty(user.PhoneNumber))
+            {
+                smsService.Send(user.PhoneNumber, text + $" {ApplicationSettings.TeacherProjectName}");
+            }
+        }
+    }
+}
