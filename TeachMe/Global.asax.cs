21ac817d80b1bbd64d32b551373dbd93edfec900
maradik <maradik@bk.ru>
2016-12-11 17:29:23 +0500

    Логирование всех необработанных исключений

--- TeachMe/Global.asax.cs
+++ TeachMe/Global.asax.cs
@@ -1,12 +1,16 @@
-﻿using log4net.Config;
+﻿using System;
+using log4net.Config;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using log4net;
 
 namespace TeachMe
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        private static readonly ILog Logger = LogManager.GetLogger("Global");
+
         protected void Application_Start()
         {
             XmlConfigurator.Configure();
@@ -15,6 +19,12 @@ namespace TeachMe
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+            Logger.Error("Необработанное исключение", exception);
         }
     }
 }
