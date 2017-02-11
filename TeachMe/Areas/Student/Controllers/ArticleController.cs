91875965050ab30d1dfb46ce72b6c2249b84237b
maradik <maradik@bk.ru>
2017-02-12 02:20:21 +0500

    Добавил статью "Заказать реферат" в StudentArea

---
+++ TeachMe/Areas/Student/Controllers/ArticleController.cs
@@ -0,0 +1,21 @@
+﻿using System.Web.Mvc;
+using TeachMe.ProjectsSupport;
+using TeachMe.Services.General;
+
+namespace TeachMe.Areas.Student.Controllers
+{
+    [AllowAnonymous]
+    public class ArticleController : StudentControllerBase
+    {
+        public ArticleController(IProjectTypeProvider projectTypeProvider,
+                                 IProjectInfoProvider projectInfoProvider) 
+            : base(projectTypeProvider, projectInfoProvider)
+        {
+        }
+
+        public ActionResult Referat()
+        {
+            return View();
+        }
+    }
+}
