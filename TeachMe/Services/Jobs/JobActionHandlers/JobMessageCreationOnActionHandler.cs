78b2a9df597739b840d9bb854ad2422257afbd7d
maradik <maradik@bk.ru>
2017-04-17 23:22:35 +0500

    Все действия пользователей над задачей записываются комментариями к задаче

---
+++ TeachMe/Services/Jobs/JobActionHandlers/JobMessageCreationOnActionHandler.cs
@@ -0,0 +1,36 @@
+﻿using TeachMe.Helpers.Settings;
+using TeachMe.Models.Jobs;
+using TeachMe.Models.Users;
+using TeachMe.Services.Notifications;
+using TeachMe.Extensions;
+using TeachMe.DataAccess.Jobs;
+
+namespace TeachMe.Services.Jobs.JobActionHandlers
+{
+    public class JobMessageCreationOnActionHandler : IJobActionCustomHandler
+    {
+        private readonly IJobMessageRepository jobMessageRepository;
+
+        public JobMessageCreationOnActionHandler(IJobMessageRepository jobMessageRepository)
+        {
+            this.jobMessageRepository = jobMessageRepository;
+        }
+
+        public void Handle(Job job, JobActionType actionType, ApplicationUser user)
+        {
+            if (actionType == JobActionType.Edit || actionType == JobActionType.Delete)
+            {
+                return;
+            }
+
+            var jobMessage = new JobMessage
+            {
+                JobId = job.Id,
+                Text = "Пользователь выполнил действие: " + actionType.GetHumanAnnotation(),
+                UserId = user.Id
+            };
+
+            jobMessageRepository.Write(jobMessage);
+        }
+    }
+}
